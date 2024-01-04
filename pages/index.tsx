329df7f91ffe72ab8dd5c534e95f2ac4bfa55393
Matheusbcy <matheusbcy@gmail.com>
2024-01-04 13:31:16 -0300

    billboard and card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
-import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorite";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,10 +23,17 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 
-export default function Home() { 
+export default function Home() {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
   return (
     <>
       <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title={"Trending Now"} data={movies} />
+        <MovieList title={"Minha Lista"} data={favorites} />
+      </div>
     </>
   );
 }
